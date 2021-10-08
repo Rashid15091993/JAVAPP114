2b1944ef0b4c99187feb686fab688a89fe7a1dc6
rashi <rashid058@mial.ru>
2021-10-08 23:04:22 +0500

    #####

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Util instance = null;
+    private static Connection connection = null;
+    private static final String URL = "jdbc:mysql://localhost:3306/userdb?useSSL=false&serverTimezone=Asia/Shanghai";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123456";
+
+    private Util() {
+        try {
+            if (null == connection || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Util getInstance() {
+        if (null == instance) {
+            instance = new Util();
+        }
+        return instance;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
 }
